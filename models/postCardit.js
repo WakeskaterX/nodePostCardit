e249beb0e1033705c4207d7ca9fd80aea8d26905
WakeskaterX <coolrider6987@gmail.com>
2014-11-16 16:41:11 -0500

    Fixed a bug with first time uploads - was bad callbacks not nested correctly

--- models/postCardit.js
+++ models/postCardit.js
@@ -10,26 +10,26 @@ exports.cardit = function(callback,imageName){
 	
 	gm(stampPath).resize(60).write('public/imageData/ref_postalStamp.jpg', function(err){
 		if (err) console.dir(err);
-	});
 
-	gm(imagePath).resize(400).write('public/imageData/ref_'+imageName, function(err){
-		if (err) console.dir(err);
-	});
+		gm(imagePath).resize(400).write('public/imageData/ref_'+imageName, function(err){
+			if (err) console.dir(err);
 
-	gm().in('-page','+0+0')
-		.in('public/imageData/ref_'+imageName)
-		.in('-page','+340+0')
-		.in(stampPath2)
-		.fontSize(24)
-		.fill('#000')
-		.drawText(20,120,'Hello From My Vacation Spot!')
-		.mosaic()
-		.write('public/imageData/'+'stampified_'+imageName, function(err){
-			if (!err){
-				callback(null,'stampified_'+imageName);
-			} else {
-				callback(err,null);
-			}
+			gm().in('-page','+0+0')
+				.in('public/imageData/ref_'+imageName)
+				.in('-page','+340+0')
+				.in(stampPath2)
+				.fontSize(24)
+				.fill('#000')
+				.drawText(20,120,'Hello From My Vacation Spot!')
+				.mosaic()
+				.write('public/imageData/'+'stampified_'+imageName, function(err){
+					if (!err){
+						callback(null,'stampified_'+imageName);
+					} else {
+						callback(err,null);
+					}
+			});
+		});
 	});
 
 	
